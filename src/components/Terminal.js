da34ea76dda551598a72c519d5886526f527b65a
Luiz Felipe <luizfelipebasile@gmail.com>
2024-07-17 22:18:56 -0300

    Add some tests to terminal and do some refactor

--- src/components/Terminal.js
+++ src/components/Terminal.js
@@ -3,6 +3,7 @@ import axios from 'axios';
 
 import './Terminal.css';
 
+
 const Terminal = ({ token }) => {
   const [command, setCommand] = useState('');
   const [terminalOutput, setTerminalOutput] = useState('');
@@ -19,7 +20,7 @@ const Terminal = ({ token }) => {
           },
         });
 
-        if (cdGetResponse.status === 200 && cdGetResponse.data.cwd) {
+        if (cdGetResponse && cdGetResponse.status === 200 && cdGetResponse.data.cwd) {
           const cwd = cdGetResponse.data.cwd;
           setTerminalOutput((prevOutput) => prevOutput + '\n' + cwd);
           setLastValidDirectory(cwd);
@@ -27,7 +28,6 @@ const Terminal = ({ token }) => {
           setTerminalOutput((prevOutput) => prevOutput + '\n' + lastValidDirectory);
         }
       } catch (error) {
-        console.error('Error fetching current directory:', error);
         setTerminalOutput((prevOutput) => prevOutput + '\n' + lastValidDirectory);
       }
     };
@@ -39,92 +39,111 @@ const Terminal = ({ token }) => {
   }, []); // Empty dependency array ensures this effect runs only once on mount
 
   useEffect(() => {
-    inputRef.current.scrollIntoView({ behavior: 'smooth' });
+    if (inputRef && inputRef.current && inputRef.current.scrollIntoView) {
+      inputRef.current.scrollIntoView({ behavior: 'smooth' });
+    }
   }, [terminalOutput]);
 
-  const handleCommandSubmit = async (e) => {
-    e.preventDefault();
+const handleHistoryCommand = async (args) => {
+  let historyCommands = '';
+  try {
+    const historyResponse = await axios.get('/api/history', {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
 
-    try {
-      if (command.trim().toLowerCase() === 'clear') {
-        setTerminalOutput('');
-        setCommand('');
-        return;
-      }
-
-      const args = command.trim().split(' ');
-      if (args[0].toLowerCase() === 'history') {
-        let historyCommands = '';
-        try {
-          const historyResponse = await axios.get('/api/history', {
-            headers: {
-              Authorization: `Bearer ${token}`,
-            },
-          });
-
-          if (historyResponse.status === 200) {
-            const limit = !isNaN(parseInt(args[1])) ? parseInt(args[1]) : historyResponse.data.length;
-            const historyData = historyResponse.data.length === limit ? historyResponse.data.slice(0, limit) :
-            historyResponse.data.reverse().slice(0, limit).reverse();; // Reverse and limit history data
-            historyCommands = historyData.map((entry, index) => `${index + 1}. ${entry.command}`).join('\n');
-            setTerminalOutput((prevOutput) => prevOutput + '\n' + historyCommands);
-          } else {
-            setTerminalOutput((prevOutput) => prevOutput + '\nCommand not found');
-          }
-        } catch (error) {
-          console.error('Error fetching command history:', error);
-          setTerminalOutput((prevOutput) => prevOutput + '\nCommand not found');
-        }
-
-        setCommand('');
-        return;
-      }
-
-      if (args[0].toLowerCase() === 'ls') {
-        try {
-          const lsResponse = await axios.get('/api/ls', {
-            params: {
-              options: args.length > 1 ? args.slice(1).join(' ') : '', // Pass remaining arguments as options
-            },
-            headers: {
-              Authorization: `Bearer ${token}`,
-            },
-          });
-
-          if (lsResponse.status === 200) {
-            setTerminalOutput((prevOutput) => prevOutput + '\n' + lsResponse?.data?.ls);
-          } else {
-            throw new Error(lsResponse.error);
-          }
-        } catch (error) {
-          console.error('Error executing ls command:', error);
-          setTerminalOutput((prevOutput) => prevOutput + '\n' + error?.response?.data?.error);
-        }
-
-        setCommand('');
-        return;
-      }
-
-      // Handle other commands like cd, etc.
-      const response = await axios.post('/api/cd', { command }, {
-        headers: { Authorization: `Bearer ${token}` }
-      });
-
-      if (response.status === 200) {
-        const cwd = response.data.cwd;
-        setTerminalOutput((prevOutput) => prevOutput + '\n' + cwd);
-        setLastValidDirectory(cwd);
-      } else {
-        setTerminalOutput((prevOutput) => prevOutput + '\nCommand not found');
-      }
-
-      setCommand('');
-    } catch (error) {
-      console.error('Command execution error:', error);
-      setTerminalOutput((prevOutput) => prevOutput + '\nCommand execution error');
-      setCommand('');
+    if (historyResponse.status === 200) {
+      const limit = !isNaN(parseInt(args[1])) ? parseInt(args[1]) : historyResponse.data.length;
+      const historyData = historyResponse.data.length === limit ? historyResponse.data.slice(0, limit) :
+        historyResponse.data.reverse().slice(0, limit).reverse(); // Reverse and limit history data
+      historyCommands = historyData.map((entry, index) => `${index + 1}. ${entry.command}`).join('\n');
+      setTerminalOutput((prevOutput) => prevOutput + '\n' + historyCommands);
+    } else {
+      setTerminalOutput((prevOutput) => prevOutput + '\nCommand not found');
     }
-  };
+  } catch (error) {
+    console.error('Error fetching command history:', error);
+    setTerminalOutput((prevOutput) => prevOutput + '\nCommand not found');
+  }
+  setCommand('');
+};
+
+const handleLsCommand = async (args) => {
+  try {
+    const lsResponse = await axios.get('/api/ls', {
+      params: {
+        options: args.length > 1 ? args.slice(1).join(' ') : '', // Pass remaining arguments as options
+      },
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
+
+    if (lsResponse.status === 200) {
+      setTerminalOutput((prevOutput) => prevOutput + '\n' + lsResponse?.data?.ls);
+    } else {
+      throw new Error(lsResponse.error);
+    }
+  } catch (error) {
+    console.error('Error executing ls command:', error);
+    setTerminalOutput((prevOutput) => prevOutput + '\n' + error?.response?.data?.error);
+  }
+  setCommand('');
+};
+
+const handleOtherCommand = async () => {
+  try {
+    const response = await axios.post('/api/cd', { command }, {
+      headers: { Authorization: `Bearer ${token}` }
+    });
+
+    if (response.status === 200) {
+      const cwd = response.data.cwd;
+      setTerminalOutput((prevOutput) => prevOutput + '\n' + cwd);
+      setLastValidDirectory(cwd);
+    } else {
+      setTerminalOutput((prevOutput) => prevOutput + '\nCommand not found');
+    }
+  } catch (error) {
+    console.error('Command execution error:', error);
+    setTerminalOutput((prevOutput) => prevOutput + '\nCommand execution error');
+  }
+  setCommand('');
+};
+const handleClearCommand = () => {
+  setTerminalOutput('');
+  setCommand('');
+};
+const handleCommandSubmit = async (e) => {
+  e.preventDefault();
+  try {
+    if (command.trim().toLowerCase() === 'clear') {
+      handleClearCommand();
+      return;
+    }
+
+    const args = command.trim().split(' ');
+    if (args[0].toLowerCase() === 'history') {
+      await handleHistoryCommand(args);
+      return;
+    }
+
+    if (args[0].toLowerCase() === 'ls') {
+      await handleLsCommand(args);
+      return;
+    }
+
+    // Handle other commands like cd, etc.
+    await handleOtherCommand();
+
+  } catch (error) {
+    console.error('Command execution error:', error);
+    setTerminalOutput((prevOutput) => prevOutput + '\nCommand execution error');
+  } finally {
+    setCommand('');
+  }
+};
 
   const handleKeyPress = (e) => {
     if (e.key === 'Enter') {
@@ -136,9 +155,9 @@ const Terminal = ({ token }) => {
     <div className="terminal">
       <h2>Terminal</h2>
       <div className="terminal-screen">
-        <pre>{terminalOutput}</pre>
+        <pre data-testid='terminal-output'>{terminalOutput}</pre>
         <div>
-          <span className='last-valid-dir'>{lastValidDirectory}$&nbsp;</span>
+          <span className='last-valid-dir' data-testid='dir' >{lastValidDirectory}$&nbsp;</span>
           <form onSubmit={handleCommandSubmit}>
             <input
               type="text"
---
+++ src/components/Terminal.test.js
@@ -0,0 +1,262 @@
+import React from 'react';
+import { render, screen, fireEvent } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+import axios from 'axios';
+import Terminal from './Terminal';
+
+jest.mock('axios');
+
+describe('Terminal Component', () => {
+  const token = 'test-token';
+
+  beforeEach(() => {
+    jest.clearAllMocks();
+  });
+
+  afterEach(() => {
+    jest.clearAllMocks();
+    jest.restoreAllMocks();
+  });
+  test('Default user directory', async () => {
+    // Render the component
+    render(<Terminal token={token} />);
+
+    // Wait for the text to appear in terminal output
+    const lastValidDirElement = await screen.findByTestId('dir');
+    expect(lastValidDirElement.textContent.trim()).toEqual('/$');
+
+  });
+
+  test('Typing and submitting a cd command', async () => {
+    // Mock axios.post response for command execution
+    const command = 'cd /home/user';
+    const lsResponse = { data: { ls: 'file1 file2 file3' } };
+    axios.post.mockResolvedValueOnce({ status: 200, data: { cwd: '/home/user' } });
+    axios.get.mockResolvedValueOnce(lsResponse);
+
+    // Render the component
+    render(<Terminal token={token} />);
+
+    // Wait for the text to appear in terminal output
+    const lastValidDirElement = await screen.findByTestId('dir');
+    expect(lastValidDirElement.textContent.trim()).toEqual('/$');
+
+    // Type the command into the input field
+    const inputField = screen.getByRole('textbox');
+    fireEvent.change(inputField, { target: { value: command } });
+
+    // Submit the form (simulate pressing Enter)
+    fireEvent.submit(inputField.closest('form'));
+    // Wait for the terminal output to update
+    const terminalOutput = await screen.findByTestId('dir');
+    expect(terminalOutput.textContent.trim()).toEqual('/home/user$');
+
+    jest.restoreAllMocks();
+  });
+
+  test('Typing and submitting a ls command', async () => {
+    // Mock axios.post response for command execution
+    const command = 'ls';
+    axios.post.mockResolvedValueOnce({ status: 200, data: { cwd: '/home/user' } });
+    axios.get.mockResolvedValueOnce({ status: 200, data: { ls: '/felipe/luiz', cwd: '/' } });
+
+    // Render the component
+    render(<Terminal token={token} />);
+
+    // Wait for the text to appear in terminal output
+    const lastValidDirElement = await screen.findByTestId('dir');
+    expect(lastValidDirElement.textContent.trim()).toEqual('/$');
+
+    // Type the command into the input field
+    const inputField = screen.getByRole('textbox');
+
+    fireEvent.change(inputField, { target: { value: 'cd /home' } });
+    fireEvent.submit(inputField.closest('form'));
+
+    await screen.findByTestId('terminal-output');
+
+    fireEvent.change(inputField, { target: { value: command } });
+
+    // Submit the form (simulate pressing Enter)
+    fireEvent.submit(inputField.closest('form'));
+    // Wait for the terminal output to update
+    const terminalOutput = await screen.findByTestId('terminal-output');
+    expect(terminalOutput.textContent.trim()).toContain('/home/user');
+
+    jest.restoreAllMocks();
+  });
+
+  test('Typing and submitting a history command', async () => {
+    // Mock axios.post response for command execution
+    const command = 'history 3';
+    axios.post.mockResolvedValueOnce({ status: 200, data: { cwd: '/home/user' } });
+    axios.get.mockResolvedValueOnce({ status: 200, data: { ls: '/felipe/luiz', cwd: '/test' } });
+    axios.get.mockResolvedValueOnce({ status: 200, data: [{command: 'cd /test', cwd: '/test/from/history'}] });
+
+    // Render the component
+    render(<Terminal token={token} />);
+
+    // Wait for the text to appear in terminal output
+    const lastValidDirElement = await screen.findByTestId('dir');
+    expect(lastValidDirElement.textContent.trim()).toEqual('/test$');
+
+    // Type the command into the input field
+    const inputField = screen.getByRole('textbox');
+
+    fireEvent.change(inputField, { target: { value: 'cd /test' } });
+    fireEvent.submit(inputField.closest('form'));
+
+    await screen.findByTestId('terminal-output');
+
+    fireEvent.change(inputField, { target: { value: command } });
+
+    // Submit the form (simulate pressing Enter)
+    fireEvent.submit(inputField.closest('form'));
+    // Wait for the terminal output to update
+    const terminalOutput = await screen.findByTestId('terminal-output');
+    expect(terminalOutput.textContent.trim()).toContain('1. cd /test');
+
+    jest.restoreAllMocks();
+  });
+
+  test('Clear command', async () => {
+    // Mock axios.post response for command execution
+    const command = 'clear';
+    axios.post.mockResolvedValueOnce({ status: 200, data: { cwd: '/home/user' } });
+  
+    // Render the component
+    render(<Terminal token={token} />);
+  
+    // Wait for the text to appear in terminal output
+    const lastValidDirElement = await screen.findByTestId('dir');
+    expect(lastValidDirElement.textContent.trim()).toEqual('/$');
+  
+    // Type the command into the input field
+    const inputField = screen.getByRole('textbox');
+    fireEvent.change(inputField, { target: { value: command } });
+  
+    // Submit the form (simulate pressing Enter)
+    fireEvent.submit(inputField.closest('form'));
+  
+    // Wait for the terminal output to update
+    const terminalOutput = await screen.findByTestId('terminal-output');
+    expect(terminalOutput.textContent.trim()).toEqual('');
+  
+    // Check that the last valid directory remains unchanged
+    const updatedLastValidDirElement = await screen.findByTestId('dir');
+    expect(updatedLastValidDirElement.textContent.trim()).toEqual('/$');
+  
+    jest.restoreAllMocks();
+  });
+
+  test('Error handling', async () => {
+    // Mock axios.post response for command execution
+    const command = 'invalid_command';
+    axios.post.mockRejectedValueOnce({ response: { status: 500, data: { error: 'Server Error' } } });
+  
+    // Render the component
+    render(<Terminal token={token} />);
+  
+    // Wait for the text to appear in terminal output
+    const lastValidDirElement = await screen.findByTestId('dir');
+    expect(lastValidDirElement.textContent.trim()).toEqual('/$');
+  
+    // Type the command into the input field
+    const inputField = screen.getByRole('textbox');
+    fireEvent.change(inputField, { target: { value: command } });
+  
+    // Submit the form (simulate pressing Enter)
+    fireEvent.submit(inputField.closest('form'));
+  
+    // Wait for the terminal output to update
+    const terminalOutput = await screen.findByTestId('terminal-output');
+    expect(terminalOutput.textContent.trim()).toContain('Command execution error');
+  
+    jest.restoreAllMocks();
+  });
+
+  test('Empty command', async () => {
+    // Render the component
+    render(<Terminal token={token} />);
+  
+    // Wait for the text to appear in terminal output
+    const lastValidDirElement = await screen.findByTestId('dir');
+    expect(lastValidDirElement.textContent.trim()).toEqual('/$');
+  
+    // Submit the form with an empty command
+    const inputField = screen.getByRole('textbox');
+    fireEvent.submit(inputField.closest('form'));
+  
+    // Wait for the terminal output to update
+    const terminalOutput = await screen.findByTestId('terminal-output');
+    expect(terminalOutput.textContent.trim()).toContain('/');
+  
+    jest.restoreAllMocks();
+  });
+  
+  test('Typing and submitting multiple commands in sequence', async () => {
+    // Mock axios responses for commands
+    const commands = [
+      { command: 'cd /home', response: { status: 200, data: { cwd: '/home' } } },
+      { command: 'ls', response: { status: 200, data: { ls: 'file1 file2 file3' } } },
+      { command: 'history', response: { status: 200, data: [{ command: 'cd /home' }, { command: 'ls' }] } }
+    ];
+  
+    axios.post.mockResolvedValueOnce(commands[0].response);
+    axios.get.mockResolvedValueOnce(commands[1].response);
+    axios.get.mockResolvedValueOnce(commands[2].response);
+  
+    // Render the component
+    render(<Terminal token={token} />);
+  
+    // Wait for the text to appear in terminal output
+    const lastValidDirElement = await screen.findByTestId('dir');
+    expect(lastValidDirElement.textContent.trim()).toEqual('/$');
+  
+    // Execute each command in sequence
+    const inputField = screen.getByRole('textbox');
+    fireEvent.change(inputField, { target: { value: commands[2].command } });
+    fireEvent.submit(inputField.closest('form'));
+    await screen.findByTestId('terminal-output');
+
+    fireEvent.change(inputField, { target: { value: commands[1].command } });
+    fireEvent.submit(inputField.closest('form'));
+    await screen.findByTestId('terminal-output');
+
+    fireEvent.change(inputField, { target: { value: commands[0].command } });
+    fireEvent.submit(inputField.closest('form'));
+    await screen.findByTestId('terminal-output');
+  
+    // Check the final output
+    const terminalOutput = await screen.findByTestId('terminal-output');
+    expect(terminalOutput.textContent.trim()).toContain('/home');
+    expect(terminalOutput.textContent.trim()).toContain('1. cd /home');
+    expect(terminalOutput.textContent.trim()).toContain('2. ls');
+  
+    jest.restoreAllMocks();
+  });
+
+
+  test('Unauthorized access', async () => {
+    // Mock axios.get response for unauthorized access
+    axios.get.mockRejectedValueOnce({ response: { status: 401, data: { error: 'Unauthorized' } } });
+    // Render the component
+    render(<Terminal token={token} />);
+  
+
+    // Type the command into the input field
+    const inputField = screen.getByRole('textbox');
+    fireEvent.change(inputField, { target: { value: 'cd /test/1' } });
+  
+    // Submit the form (simulate pressing Enter)
+    fireEvent.submit(inputField.closest('form'));
+
+    // Wait for the text to appear in terminal output
+    const terminalOutput = await screen.findByTestId('terminal-output');
+    expect(terminalOutput.textContent.trim()).toContain('Command execution error');
+  
+    jest.restoreAllMocks();
+  });
+  
+
+});
